17f717c06de5e20fe4be0305897696589f72882b
Backend_Vitalii_D <vitalii.dykhal@abz.agency>
2024-10-20 16:47:40 +0300

    codecrafters submit [skip ci]

--- commands/exaptions.go
+++ commands/exaptions.go
@@ -9,7 +9,6 @@ func CommandNotFound(input string) string {
 	value, isOk := Collections[input]
 	if !isOk {
 		fmt.Fprint(os.Stdout, input[:len(input)-1]+": command not found\n")
-		os.Exit(1)
 	}
 
 	return value
